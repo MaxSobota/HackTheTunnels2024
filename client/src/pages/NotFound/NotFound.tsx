931e090d0f746bbd68dc51c43b9c07f35c260d4c
Max <maxsobot05@gmail.com>
2024-10-19 15:31:42 -0400

    Added funny

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -6,6 +6,12 @@ function NotFound() { // new for challenge 4
     <Layout title={"No page for you"}>
       <h1>Instead of a page, here's a frog</h1>
       <img src="/frog.jpg" alt="frog" />
+      <audio controls autoPlay loop><source src="buzzer.mp3" type="audio/mp3"/></audio>
+      <audio controls autoPlay><source src="explosion.mp3" type="audio/mp3"/></audio>
+      <audio controls autoPlay loop><source src="fart.mp3" type="audio/mp3"/></audio>
+      <audio controls autoPlay><source src="goofy.mp3" type="audio/mp3"/></audio>
+      <audio controls autoPlay><source src="laugh.mp3" type="audio/mp3"/></audio>
+      <audio controls autoPlay loop><source src="sus.mp3" type="audio/mp3"/></audio>
     </Layout>
   );
 }
