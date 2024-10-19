314ac1a8cffd16f2d1047da0189fb58866b3ed77
Max <maxsobot05@gmail.com>
2024-10-19 14:56:29 -0400

    Completed challenge 9

--- client/src/layouts/Central/Banner/Banner.tsx
+++ client/src/layouts/Central/Banner/Banner.tsx
@@ -6,6 +6,8 @@ function Banner() { // Edited banner image for challenge 3
       <img src="/nerd.png" alt="nerd" />
       <div className="Banner__seperator"></div>
       <div className="Banner__title">Carleton's Least User Friendly Site</div>
+      <div className="Banner__seperator"></div>
+      <div className="Banner__subway"><img src="/subway.gif"alt="subway surfers"/></div>
     </div>
   );
 }
